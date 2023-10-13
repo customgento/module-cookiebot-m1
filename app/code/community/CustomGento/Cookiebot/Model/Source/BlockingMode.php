99b896ebb5b9cf041beab80dc2d37c7946388340
norgeindian <ole@customgento.com>
2023-10-13 10:09:02 +0200

    Reformat, DEV-137

--- app/code/community/CustomGento/Cookiebot/Model/Source/BlockingMode.php
+++ app/code/community/CustomGento/Cookiebot/Model/Source/BlockingMode.php
@@ -6,9 +6,9 @@ class CustomGento_Cookiebot_Model_Source_BlockingMode
 {
     public function toOptionArray(): array
     {
-        return array(
-            array('value' => 'auto', 'label'=>Mage::helper('adminhtml')->__('Auto')),
-            array('value' => 'manual', 'label'=>Mage::helper('adminhtml')->__('Manual')),
-        );
+        return [
+            ['value' => 'auto', 'label' => Mage::helper('adminhtml')->__('Auto')],
+            ['value' => 'manual', 'label' => Mage::helper('adminhtml')->__('Manual')],
+        ];
     }
 }
